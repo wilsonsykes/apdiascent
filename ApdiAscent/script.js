9bf3d0b0d5b69fc8186709191a3609cf2769f64c
wilsonsykes <coresykes05@gmail.com>
2025-05-06 13:38:20 +0800

    Update script.js

--- ApdiAscent/script.js
+++ ApdiAscent/script.js
@@ -1,6 +1,6 @@
 // ============ CONFIG ============
 const PDF_LOG_URL = 'https://raw.githubusercontent.com/wilsonsykes/apdiascent/main/ApdiAscent/data/pdf-log.json?t=' + Date.now();
-const GENERATE_PDFS_WEBHOOK = 'https://script.google.com/macros/s/AKfycbxwaOnGbD81HtjaeH04sYs1bkSn1JaYGZ8JaiWCikAx12xpKlyoYXC53C3xQ9UsQ7zc0g/exec';
+const GENERATE_PDFS_WEBHOOK = 'https://script.google.com/macros/s/AKfycbxUXVvjh6iq9t2g1cNnWx9UR-UJ6op40YXtZQN0huttahKgLKcWSO25ul2NEToJhdxaww/exec';
 const SEND_ALL_WEBHOOK = 'https://n8n.apdi2025.site/webhook/c223cbc5-4c07-445c-ac0b-ebbe309f95be';
 
 
